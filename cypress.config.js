9dabf9c6482b82596f2a9b93b66b12fe0d1e8cb7
weverson07 <wefferson07@hotmail.com>
2025-05-24 22:53:44 -0300

    atualização do código e pipeline.

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,13 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: false,
+    json: true
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // configure plugins here se necessário
